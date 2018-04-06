9e5ce7e1057808adff3d90f8c0ec20604123485a
Anthony Robert <tonylord.ar@gmail.com>
2018-04-05 21:35:35 -0400

    Push some things

---
+++ Assets/_CodePanda/Scripts/Item/Items.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour {
+
+	public enum EItem
+    {
+        Obj1,
+        Obj2,
+        Obj3,
+        Obj4,
+        Obj5,
+        Obj6,
+        Obj7,
+        Obj8
+    }
+
+    private void Recipes()
+    {
+        
+    }
+}
