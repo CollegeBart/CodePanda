3842f136f878b7c9758e0c0c87a653d62cabb3f8
Majorbuzzz <36974606+Majorbuzzz@users.noreply.github.com>
2018-04-05 21:12:28 -0400

    Ajout prefab Ingredient

---
+++ Assets/_CodePanda/Scripts/Item/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿// Put this in [...]\Unity2017.3.1f1\Editor\Data\Resources\ScriptTemplates
+
+using UnityEngine;
+
+namespace ca.codepanda
+{
+	public class Ingredient : MonoBehaviour
+    {
+        public enum IngredientsType
+        {
+            Chose1 = 1,
+            Chose2 = 2
+        }
+
+        public IngredientsType Type;
+    }
+}
