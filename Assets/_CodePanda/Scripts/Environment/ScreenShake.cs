f0425f3f998f3503ab64a34e90516ce8583208de
Anthony Robert <tonylord.ar@gmail.com>
2018-04-05 20:14:28 -0400

    ScreenShake as beeen pushed

--- Assets/_CodePanda/Scripts/Environment/ScreenShake.cs
+++ Assets/_CodePanda/Scripts/Environment/ScreenShake.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class ScreenShake : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Transform of the camera to shake. Grabs the gameObject's transform
+    // if null.
+    public Transform m_CamTransform;
+
+    // How long the object should shake for.
+    public float m_ShakeDuration = 0f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float m_ShakeAmount = 0.7f;
+    public float m_DecreaseFactor = 1.0f;
+
+    Vector3 originalPos;
+
+    void Awake()
+    {
+        if (m_CamTransform == null)
+        {
+            m_CamTransform = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
+
+    void OnEnable()
+    {
+        originalPos = m_CamTransform.localPosition;
+    }
+
+    void Update()
+    {
+        if (m_ShakeDuration > 0)
+        {
+            m_CamTransform.localPosition = originalPos + Random.insideUnitSphere * m_ShakeAmount;
+
+            m_ShakeDuration -= Time.deltaTime * m_DecreaseFactor;
+        }
+        else
+        {
+            m_ShakeDuration = 0f;
+            m_CamTransform.localPosition = originalPos;
+        }
+    }
 }
